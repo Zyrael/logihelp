356defd75b3eb4d5c1b043fab7370f223b2984f3
Zyrael <zyrael@icloud.com>
2023-02-10 16:00:08 +0300

    added deleting supplier option

--- server/server.js
+++ server/server.js
@@ -21,7 +21,10 @@ const resolvers = {
   },
 
   Mutation: {
-    addSupplier: async (_, { name, url, additionalData, addresses }) => {
+    addSupplier: async (
+      _,
+      { name, url, additionalData, addresses = "[]", contacts = "[]" }
+    ) => {
       const supplier = await prisma.supplier.create({
         data: { name, url, additionalData },
       });
@@ -36,10 +39,23 @@ const resolvers = {
         });
       });
 
+      JSON.parse(contacts).forEach(async (contact) => {
+        await prisma.contact.create({
+          data: {
+            name: contact.name,
+            phoneNumber: contact.phoneNumber,
+            supplierId: supplier.id,
+          },
+        });
+      });
+
       return supplier;
     },
 
-    updateSupplier: async (_, { id, url, additionalData }) => {
+    updateSupplier: async (
+      _,
+      { id, url, additionalData, addresses = "[]", contacts = "[]" }
+    ) => {
       const supplier = await prisma.supplier.update({
         where: { id: parseInt(id, 10) },
         data: {
@@ -48,23 +64,49 @@ const resolvers = {
         },
       });
 
-      return supplier;
-    },
-
-    addAddressToSupplier: async (_, { id, name, address }) => {
-      const supplier = await prisma.supplier.update({
-        where: { id: parseInt(id, 10) },
-        data: {
-          addresses: {
-            create: {
-              name,
-              address,
-            },
+      JSON.parse(addresses).forEach(async (address) => {
+        await prisma.address.create({
+          data: {
+            name: address.name,
+            address: address.address,
+            supplierId: parseInt(id, 10),
           },
-        },
+        });
+      });
+
+      JSON.parse(contacts).forEach(async (contact) => {
+        await prisma.contact.create({
+          data: {
+            name: contact.name,
+            phoneNumber: contact.phoneNumber,
+            supplierId: parseInt(id, 10),
+          },
+        });
       });
 
       return supplier;
+    },
+
+    deleteSupplier: async (_, { id }) => {
+      await prisma.address.deleteMany({
+        where: {
+          supplierId: parseInt(id, 10),
+        },
+      });
+
+      await prisma.contact.deleteMany({
+        where: {
+          supplierId: parseInt(id, 10),
+        },
+      });
+
+      await prisma.supplier.delete({
+        where: {
+          id: parseInt(id, 10),
+        },
+      });
+
+      return true;
     },
   },
 };
