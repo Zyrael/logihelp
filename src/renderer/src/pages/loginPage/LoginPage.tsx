0d878bcc674d7d76bd4797febf5c239bef22f61e
Zyrael <zyrael@icloud.com>
2023-09-14 23:04:32 +0300

    some styling

--- src/renderer/src/pages/loginPage/LoginPage.tsx
+++ src/renderer/src/pages/loginPage/LoginPage.tsx
@@ -5,25 +5,33 @@ interface Props {
 
 export function LoginPage({ url, updateDB }: Props): JSX.Element {
   return (
-    <div className="flex flex-col justify-center items-center h-full bg-purple-300">
-      <div className="flex">
-        <div className="text-3xl flex items-center select-none">База:</div>
-        {url ? (
-          <div className="text-2xl ml-4 bg-slate-50 px-4 py-2 rounded-md select-none">{url}</div>
-        ) : (
+    <div className="flex flex-col justify-center items-center h-full bg-neutral-light  font-roboto">
+      <div className="bg-primary text-white py-16 px-14 rounded-3xl flex flex-col items-center min-w-[550px]">
+        <div className="text-2xl font-bold">Войти в справочник</div>
+        <div className="w-full mt-8">
+          <div className="text-2xl select-none">База:</div>
+          {url ? (
+            <div className="text-2xl mt-2 rounded-md select-none font-light">{url}</div>
+          ) : (
+            <button
+              type="button"
+              onClick={(): void => updateDB()}
+              className="mt-4 bg-white text-2xl px-14 py-3 rounded-md text-primary font-bold"
+            >
+              ДОБАВИТЬ
+            </button>
+          )}
+        </div>
+
+        <div className="flex justify-center mt-12">
           <button
             type="button"
-            onClick={(): void => updateDB()}
-            className="ml-4 bg-white text-xl px-4 py-2 rounded-md"
+            disabled={!url}
+            className="text-2xl px-14 py-3 rounded-md bg-white text-primary font-bold"
           >
-            Добавить...
+            ВХОД
           </button>
-        )}
-      </div>
-      <div className="flex justify-center mt-4">
-        <button type="button" disabled={!url} className="bg-white text-xl px-4 py-2 rounded-md">
-          Войти
-        </button>
+        </div>
       </div>
     </div>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        'primary': '#2871ff',
+        'neutral': '#7facf0',
+        'neutral-dark': '#5580c2',
+        'neutral-light': '#bddfff',
+      },
+      fontFamily: {
+        roboto: ['Roboto', 'sans-serif']
+      }
+    },
+
   },
   plugins: []
 }
